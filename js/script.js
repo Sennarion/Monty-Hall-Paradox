1cc9ac9fa3c684bb28002fb9276a8cd9bd6fa12b
Serhii Reznichenko <81872621+Sennarion@users.noreply.github.com>
2022-11-01 16:57:43 +0200

    add main js

--- js/script.js
+++ js/script.js
@@ -1,94 +1,99 @@
-const wrapperRef = document.querySelector('.doors-wrapper');
-const doorsRef = document.querySelectorAll('.door');
-const percentRef = document.querySelector('.percent');
+const refs = {
+  doorsWrapperRef: document.querySelector('.doors-wrapper'),
+  doorsRef: document.querySelectorAll('.door-wrapper'),
+  percentRef: document.querySelector('.percent'),
+  resultRef: document.querySelector('.result'),
+  imageRef: document.querySelector('.modal-img'),
+  buttonRef: document.querySelector('.button'),
+  backdropRef: document.querySelector('.backdrop'),
+};
 
-const resultRef = document.querySelector('.result');
-const buttonRef = document.querySelector('.try-again');
-
-const backdropRef = document.querySelector('.backdrop');
+const staticsArr = [];
 
 function createRandomNumber() {
   return Math.floor(Math.random() * 3 + 1);
 }
 
-const staticsArr = [];
-
-wrapperRef.addEventListener('click', startGame, { once: true });
-
-function startGame(e) {
-  if (e.target === wrapperRef) {
-    return;
-  }
-
-  const randomNumber = createRandomNumber();
-
-  doorsRef.forEach(el => {
-    if (Number(el.dataset.number) === randomNumber) {
-      console.log(el.dataset.number);
-      el.closest('.door-wrapper').classList.add('winning');
-    }
-  });
-
-  const wrongDoor = findWrongDoor(e, randomNumber);
-
-  openDoor(wrongDoor);
-
-  wrapperRef.addEventListener('click', finishGame, {
-    once: true,
-  });
+function toggleModal() {
+  refs.backdropRef.classList.toggle('active');
 }
 
-function finishGame(e) {
-  if (e.target === wrapperRef) {
-    return;
-  }
-  e.target.classList.add('open');
-  if (e.target.closest('.door-wrapper').classList.contains('winning')) {
-    staticsArr.push(1);
-    resultRef.textContent = 'You won :)';
-  } else {
-    staticsArr.push(0);
-    resultRef.textContent = 'You lose :(';
-  }
-  percentRef.textContent = calcStatistics();
-  toggleModal();
+function calcStatistics() {
+  const trueResults = staticsArr.filter(el => el === 1);
+  return ((trueResults.length / staticsArr.length) * 100).toFixed();
 }
 
-function findWrongDoor(door, number) {
-  const wrongDoors = [...doorsRef].filter(
+function findRandomWrongDoor(door, number) {
+  const wrongDoors = [...refs.doorsRef].filter(
     el =>
-      Number(el.dataset.number) !== Number(door.target.dataset.number) &&
+      Number(el.dataset.number) !== Number(door.target.closest('.door-wrapper').dataset.number) &&
       Number(el.dataset.number) !== number
   );
 
   return wrongDoors[Math.floor(Math.random() * wrongDoors.length)];
 }
 
-function calcStatistics() {
-  const trueResults = staticsArr.filter(el => el === 1);
-  return (trueResults.length / staticsArr.length) * 100;
+function findWinningDoor(number) {
+  return [...refs.doorsRef].find(el => Number(el.dataset.number) === number);
 }
 
-function openDoor(door) {
-  door.classList.add('open');
-}
+function startGame(e) {
+  if (e.target === refs.doorsWrapperRef) {
+    return;
+  }
 
-function resetGame() {
-  wrapperRef.removeEventListener('click', startGame);
-  doorsRef.forEach(el => {
-    if (el.classList.contains('open')) {
-      el.classList.remove('open');
-    }
-    if (el.closest('.door-wrapper').classList.contains('winning')) {
-      el.closest('.door-wrapper').classList.remove('winning');
-    }
+  const randomNumber = createRandomNumber();
+
+  const wrongDoor = findRandomWrongDoor(e, randomNumber);
+  wrongDoor.classList.add('open');
+
+  const winningDoor = findWinningDoor(randomNumber);
+  winningDoor.classList.add('winning');
+
+  refs.doorsWrapperRef.addEventListener('click', finishGame, {
+    once: true,
   });
-  wrapperRef.addEventListener('click', startGame, { once: true });
+}
+
+function finishGame(e) {
+  if (e.target === refs.doorsWrapperRef) {
+    return;
+  }
+
+  e.target.closest('.door-wrapper').classList.add('open');
+
+  if (e.target.closest('.door-wrapper').classList.contains('winning')) {
+    staticsArr.push(1);
+    refs.resultRef.textContent = 'Ти виграв автомобіль :)';
+    refs.imageRef.src = './images/car.svg';
+  } else {
+    staticsArr.push(0);
+    refs.resultRef.textContent = 'Ти програв, тому тримай цукерку :(';
+    refs.imageRef.src = './images/candy.svg';
+  }
+
+  const result = calcStatistics();
+  refs.percentRef.textContent = `${result}%`;
+
   toggleModal();
 }
 
-buttonRef.addEventListener('click', resetGame);
+function resetGame() {
+  refs.doorsWrapperRef.removeEventListener('click', startGame);
 
-function toggleModal() {
-  backdropRef.classList.toggle('active');
+  refs.doorsRef.forEach(el => {
+    if (el.classList.contains('open')) {
+      el.classList.remove('open');
+    }
+    if (el.classList.contains('winning')) {
+      el.classList.remove('winning');
+    }
+  });
+
+  refs.doorsWrapperRef.addEventListener('click', startGame, { once: true });
+
+  toggleModal();
 }
+
+refs.doorsWrapperRef.addEventListener('click', startGame, { once: true });
+refs.buttonRef.addEventListener('click', resetGame);
